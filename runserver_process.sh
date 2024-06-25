ac0ed033ef10457385388472b7ac38119526d5f7
Ken Anargya Alkausar <92387421+kenanargya@users.noreply.github.com>
2024-06-26 02:43:03 +0700

    add files

---
+++ runserver_process.sh
@@ -0,0 +1,6 @@
+#jalankan 5 server thread
+
+python3 server_process_pool_http.py 8011 &
+python3 server_process_pool_http.py 8012 &
+python3 server_process_pool_http.py 8013 &
+python3 server_process_pool_http.py 8014 &
